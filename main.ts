0ea53ad2670f417e55ebc94ad424b3f78fbbcf7e
Luca Tabone <taboneluca3@gmail.com>
2024-07-07 16:16:57 +0200

    chore: mvp

--- main.ts
+++ main.ts
@@ -1,68 +1,24 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { FMG } from "azgaar-fmg-parser";
+import { App, Modal, Plugin, PluginSettingTab, Setting } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
+interface AzgaarObsidianPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: AzgaarObsidianPluginSettings = {
+	mySetting: "default",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AzgaarObsidianPlugin extends Plugin {
+	settings: AzgaarObsidianPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			id: "azgaar-obsidian-plugin-map-importer",
+			name: "Import Map",
+			callback: () => new MapImporterModal(this.app).open(),
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
@@ -70,20 +26,24 @@ export default class MyPlugin extends Plugin {
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+			console.log("click", evt);
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerInterval(
+			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000),
+		);
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData(),
+		);
 	}
 
 	async saveSettings() {
@@ -91,44 +51,114 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
+class MapImporterModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+
+		contentEl.createEl("h1", { text: "Choose Map File" });
+		const fileEl = contentEl.createEl("input", { type: "file" });
+		const buttonEl = contentEl.createEl("button", { text: "Import" });
+
+		buttonEl.addEventListener("click", async () => {
+			const file = fileEl.files?.[0];
+
+			if (!file) return;
+
+			fileEl.disabled = true;
+			buttonEl.disabled = true;
+
+			const content = await file.text();
+
+			const fmg = FMG.parse({ data: content });
+
+			await Promise.all([
+				this.app.vault.createFolder("./Burgs"),
+				this.app.vault.createFolder("./States"),
+				this.app.vault.createFolder("./Cultures"),
+				this.app.vault.createFolder("./Provinces"),
+				this.app.vault.createFolder("./Religions"),
+			]);
+
+			await Promise.all(
+				Object.values(fmg.burgs).map((burg) =>
+					this.app.vault.create(`./Burgs/${burg.name}.md`, burg.name),
+				),
+			);
+
+			await Promise.all(
+				Object.values(fmg.states).map((state) =>
+					this.app.vault.create(
+						`./States/${state.name}.md`,
+						state.name,
+					),
+				),
+			);
+
+			await Promise.all(
+				Object.values(fmg.cultures).map((culture) =>
+					this.app.vault.create(
+						`./Cultures/${culture.name}.md`,
+						culture.name,
+					),
+				),
+			);
+
+			await Promise.all(
+				Object.values(fmg.provinces).map((province) =>
+					this.app.vault.create(
+						`./Provinces/${province.name}.md`,
+						province.name,
+					),
+				),
+			);
+
+			await Promise.all(
+				Object.values(fmg.religions).map((religion) =>
+					this.app.vault.create(
+						`./Religions/${religion.name}.md`,
+						religion.name,
+					),
+				),
+			);
+
+			this.close();
+		});
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: AzgaarObsidianPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: AzgaarObsidianPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					}),
+			);
 	}
 }
