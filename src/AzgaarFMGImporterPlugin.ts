06af108fe7c9698efccd591a7281f84d2f84f5dd
Luca Tabone <taboneluca3@gmail.com>
2024-07-14 16:16:28 +0200

    chore: remove settings

--- src/AzgaarFMGImporterPlugin.ts
+++ src/AzgaarFMGImporterPlugin.ts
@@ -1,37 +1,14 @@
 import { Plugin } from "obsidian";
-import { SettingTab } from "./SettingTab";
 import { MapImporterModal } from "./MapImporterModal";
 
-interface AzgaarFMGImporterPluginSettings {
-  mySetting: string;
-}
-
-const DEFAULT_SETTINGS: AzgaarFMGImporterPluginSettings = {
-  mySetting: "default",
-};
-
 export class AzgaarFMGImporterPlugin extends Plugin {
-  settings: AzgaarFMGImporterPluginSettings;
-
   async onload() {
-    await this.loadSettings();
-
     this.addCommand({
       id: "azgaar-fmg-importer-plugin-importer",
       name: "Import Map",
       callback: () => new MapImporterModal(this.app).open(),
     });
-
-    this.addSettingTab(new SettingTab(this.app, this));
   }
 
   onunload() {}
-
-  async loadSettings() {
-    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-  }
-
-  async saveSettings() {
-    await this.saveData(this.settings);
-  }
 }
--- src/MapImporterModal.ts
+++ src/MapImporterModal.ts
@@ -21,53 +21,55 @@ export class MapImporterModal extends Modal {
     const buttonEl = contentEl.createEl("button", { text: "Import" });
 
     buttonEl.addEventListener("click", async () => {
-      const file = fileEl.files?.[0];
+      try {
+        const file = fileEl.files?.[0];
 
-      if (!file) return;
+        if (!file) return;
 
-      fileEl.disabled = true;
-      buttonEl.disabled = true;
+        fileEl.disabled = true;
+        buttonEl.disabled = true;
 
-      const content = await file.text();
+        const content = await file.text();
 
-      const { fmg, info } = FMG.parse({ data: content });
+        const { fmg, info } = FMG.parse({ data: content });
 
-      console.log(info, fmg);
+        ["burgs", "states", "cultures", "provinces", "religions"].forEach(
+          (entity) => {
+            new Notice(
+              `Parsed ${info[entity].stored} from ${info[entity].total} ${entity}`,
+            );
+          },
+        );
 
-      ["burgs", "states", "cultures", "provinces", "religions"].forEach(
-        (entity) => {
-          new Notice(
-            `Parsed ${info[entity].stored} from ${info[entity].total} ${entity}`,
-          );
-        },
-      );
+        await Promise.all([
+          generateCultures({
+            fmg,
+            app: this.app,
+          }),
 
-      await Promise.all([
-        generateCultures({
-          fmg,
-          app: this.app,
-        }),
+          generateProvinces({
+            fmg,
+            app: this.app,
+          }),
 
-        generateProvinces({
-          fmg,
-          app: this.app,
-        }),
+          generateReligions({
+            fmg,
+            app: this.app,
+          }),
 
-        generateReligions({
-          fmg,
-          app: this.app,
-        }),
+          generateBurgs({
+            fmg,
+            app: this.app,
+          }),
 
-        generateBurgs({
-          fmg,
-          app: this.app,
-        }),
-
-        generateStates({
-          fmg,
-          app: this.app,
-        }),
-      ]);
+          generateStates({
+            fmg,
+            app: this.app,
+          }),
+        ]);
+      } catch {
+        new Notice("Failed to parse .map file");
+      }
 
       this.close();
     });
